3f6fbc1f30abe8f65043a7065614c126a15684a6
Horus <horusmoro2015@gmail.com>
2020-07-03 17:17:54 -0300

    Se agrega el modulo NyTimesReviewDataModule.

---
+++ src/main/java/ayds/cuckoo/nytimes/external/nytimes/NyTimesReviewDataModule.kt
@@ -0,0 +1,16 @@
+package ayds.cuckoo.nytimes.external.nytimes
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+object NyTimesReviewDataModule{
+
+    private val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.nytimes.com/svc/movies/v2/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+
+    private fun getNyTimesAPI(): NYTimesAPI = retrofit.create(NYTimesAPI::class.java)
+
+    val nyTimesService = NYTimesService(getNyTimesAPI(), NYTimesResponseToNYTimesMovieResolverImpl())
+}
